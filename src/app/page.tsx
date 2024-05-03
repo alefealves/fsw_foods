6ca8e35b5bb32fe6c5524a4b7e21a12c5c803a5c
Álefe Alves <alefealves@hotmail.com>
2024-05-02 21:58:33 -0400

    feat: add initial search component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 import Header from "./_components/header";
+import Search from "./_components/search";
 
 function Home() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <div className="px-5 pt-6">
+        <Search />
+      </div>
+    </>
+  );
 }
 
 export default Home;
