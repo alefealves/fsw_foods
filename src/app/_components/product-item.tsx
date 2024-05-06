3d6c4c8d995346e8de01d143909abec8930edb02
Álefe Alves <alefealves@hotmail.com>
2024-05-05 21:18:12 -0400

    feat: add cart and cart item components

--- src/app/_components/product-item.tsx
+++ src/app/_components/product-item.tsx
@@ -38,7 +38,7 @@ const ProductItem = ({ product, className }: ProductItemProps) => {
           {product.discountPercentage && (
             <div className="absolute left-2 top-2 flex items-center gap-[2px] rounded-full bg-primary px-2 py-[2px] text-white">
               <ArrowDownIcon size={12} />
-              <span className="text=xs font-semibold">
+              <span className="text-xs font-semibold">
                 {product.discountPercentage}%
               </span>
             </div>
